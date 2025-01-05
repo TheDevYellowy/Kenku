aeb0dc137003080912ff34b34c4dee1ad530e01a
TheDevYellowy <yellowycorn6963@gmail.com>
2025-01-05 02:18:32 -0600

    build whisper.cpp and get the recording 99% done

--- Client.js
+++ Client.js
@@ -1,68 +1,240 @@
-const { EndBehaviorType } = require("@discordjs/voice");
+const {
+  EndBehaviorType,
+  createAudioPlayer,
+  createAudioResource,
+} = require("@discordjs/voice");
 const { Client, IntentsBitField } = require("discord.js");
-const { opus } = require('prism-media')
-const vosk = require("vosk");
+const { opus } = require("prism-media");
+const ffmpeg = require("fluent-ffmpeg");
+const cp = require("child_process");
+const shell = require("shelljs");
 const fs = require("fs");
 
 module.exports = class extends Client {
-    constructor() {
-        super({
-            intents: [
-                IntentsBitField.Flags.GuildVoiceStates,
-                IntentsBitField.Flags.MessageContent,
-                IntentsBitField.Flags.GuildMessages,
-                IntentsBitField.Flags.GuildMembers
-            ]
-        });
+  constructor() {
+    super({
+      intents: [
+        "GuildVoiceStates",
+        "MessageContent",
+        "MessageContent",
+        "GuildMessages",
+        "GuildMembers",
+        "Guilds",
+      ],
+    });
 
-        /** @type {Record<String, import("@discordjs/voice").AudioReceiveStream} */
-        this.subscriptions = {};
-        /** @type {null | import("@discordjs/voice").VoiceConnection} */
-        this.connection = null;
+    /** @type {Record<String, import("@discordjs/voice").AudioReceiveStream} */
+    this.subscriptions = {};
+    /** @type {null | import("@discordjs/voice").VoiceConnection} */
+    this.connection = null;
+    /** @type {Record<number, NodeJS.Timeout>} */
+    this.timeouts = {};
+    this.model = "large-v3-q5_0.bin";
 
-        this.words = [];
+    this.words = [];
 
-        this.index = 0;
+    this.index = 0;
 
-        this.init();
+    this.init();
+  }
+
+  init() {
+    const words = fs.readdirSync("./words");
+    const temp = fs.readdirSync("./temp");
+
+    for (const file of temp) {
+      fs.rmSync(`./temp/${file}`);
     }
 
-    init() {
-        this.model = vosk.Model("./model");
+    for (const word of words) {
+      this.words.push(word.split(".")[0]);
+    }
+  }
 
-        const words = fs.readdirSync("./words");
-        for (const word in words) {
-            this.words.push(word.split(".")[0]);
+  whisper(index) {
+    return new Promise((resolve, reject) => {
+      try {
+        shell.exec(
+          `"./whisper/main.exe" -ml 1 -m ./whisper/${this.model} -f ./temp/${index}.wav`,
+          { silent: true, async: false },
+          (code, stdout, stderr) => {
+            if (code == 0) resolve(stdout);
+            else reject(stderr);
+          }
+        );
+
+        // cp.exec(
+        //   `"./whisper/main.exe" -ml 1 -m ./whisper/medium.bin -f ./temp/${index}.wav`,
+        //   (err, stdout, stderr) => {
+        //     console.log({ err, stdout, stderr });
+        //     if (err == null) resolve(stdout);
+        //   }
+        // );
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  formatTranscript(vtt) {
+    // 1. separate lines by matching the format like "[00:03:04.000 --> 00:03:13.000]   XXXXXX"
+    const lines = vtt.match(/\[[0-9:.]+\s-->\s[0-9:.]+\].*/g);
+    if (lines == null) return {};
+
+    // 2. remove the first line, which is empty
+    lines.shift();
+
+    // 3. convert each line into an object
+    return lines.map((line) => {
+      // 3a. split ts from speech
+      let [timestamp, speech] = line.split("]  "); // two spaces (3 spaces doesn't work with punctuation like period . )
+
+      // 3b. remove the open bracket of timestamp
+      timestamp = timestamp.substring(1);
+
+      // 3c. split timestamp into begin and end
+      const [start, end] = timestamp.split(" --> ");
+
+      // 3d. remove \n from speech with regex
+      speech = speech.replace(/\n/g, "");
+
+      // 3e. remove beginning space
+      speech = speech.replace(" ", "");
+
+      return { start, end, speech };
+    });
+  }
+
+  /** @param {any[]} transcript */
+  parse(transcript) {
+    const pass = [".", ",", "...", "-"];
+    let i = 0;
+    let transLen = transcript.length;
+    let copy = Array.from(transcript);
+
+    while (i < transLen) {
+      const data = copy[i];
+      if (pass.includes(data.speech)) continue;
+      if (data.speech.startsWith("'")) {
+        const prefix = copy[i - 1];
+        prefix.speech += data.speech;
+        prefix.speech = prefix.speech.toLowerCase();
+        prefix.end = data.end;
+        copy.splice(i, 1);
+      } else {
+        copy[i].speech = copy[i].speech.toLowerCase();
+      }
+
+      transLen = copy.length;
+      i++;
+    }
+
+    return copy;
+  }
+
+  killStream(stream) {
+    if (stream.writableLength > 0) {
+      setTimeout(() => {
+        this.killStream(stream);
+      }, 500);
+    } else stream.destroy();
+  }
+
+  /**
+   * @param {string} userId
+   */
+  subscribe(userId) {
+    if (userId == this.user.id) return;
+    if (this.index >= 15) this.index = 0;
+    const i = this.index;
+    this.index++;
+
+    const stream = this.connection.receiver
+      .subscribe(userId, {
+        end: EndBehaviorType.Manual,
+        emitClose: true,
+      })
+      .pipe(
+        new opus.Decoder({
+          rate: 16000,
+          channels: 2,
+          frameSize: 960,
+        })
+      )
+      .pipe(fs.createWriteStream(`./temp/${i}.pcm`));
+
+    setTimeout(() => {
+      console.log("Killing stream");
+      this.killStream(stream);
+    }, 7500);
+
+    stream.on("close", async () => {
+      ffmpeg(`./temp/${i}.pcm`)
+        .inputOption(["-f s16le", "-ar 16k", "-ac 2"])
+        .output(`./temp/${i}.wav`)
+        .run();
+
+      console.log(`Running wav file through whisper`);
+
+      try {
+        const trans = await this.whisper(i);
+        const format = this.formatTranscript(trans);
+        const parsed = this.parse(format);
+
+        console.log(`Parsed audio file`);
+
+        for (const value of parsed) {
+          if (this.words.includes(value.speech)) return;
+
+          this.words.push(value.speech);
+          ffmpeg(`./temp/${i}.wav`)
+            .inputOption([`-ss ${value.start}`, `-to ${value.end}`])
+            .output(`./words/${value.speech}.wav`)
+            .run();
         }
+
+        fs.rmSync(`./temp/${i}.pcm`);
+        fs.rmSync(`./temp/${i}.wav`);
+      } catch (error) {
+        console.log(`[Whisper] Error: ${error}`);
+      }
+    });
+
+    // command to split: ffmpeg -ss 10 -to 16 -i index.wav output.wav
+  }
+
+  /** @param {string} sentence */
+  say(sentence) {
+    sentence = sentence.toLowerCase();
+    const player = createAudioPlayer();
+    const con = this.connection.subscribe(player);
+
+    player.on("debug", (message) => {
+      console.log(message);
+    });
+
+    player.on("error", (err) => {
+      console.error(`Error playing audio: ${err}`);
+    });
+
+    this.makeAudio(sentence.split(" "));
+    const resource = createAudioResource("./sentence.wav");
+    player.play(resource);
+
+    con.unsubscribe();
+    fs.rmSync("./sentence.wav");
+  }
+
+  makeAudio(words) {
+    const out = fs.createWriteStream("./sentence.wav");
+    for (const word of words) {
+      const input = fs.createReadStream(`./words/${word}.wav`);
+      input.pipe(out, { end: false });
+      const _ = fs.createReadStream(`./words/-.wav`);
+      _.pipe(out, { end: false });
     }
 
-    /**
-     * @param {string} userId 
-     */
-    subscribe(userId) {
-        const i = this.index;
-        this.index++;
-
-        const rec = new vosk.Recognizer({ sampleRate: 48000, model: this.model });
-        const stream = this.connection.receiver.subscribe(userId, { end: EndBehaviorType.AfterSilence })
-        .pipe(new opus.Decoder({
-            rate: 48000,
-            channels: 2,
-            frameSize: 120
-        }))
-
-        stream.on("data", (chunk) => {
-            if(rec.acceptWaveform(chunk)) {
-                console.log(rec.result());
-            } else {
-                console.log(rec.partialResult());
-            }
-
-            console.log(rec.finalResult());
-        });
-
-        stream.on("end", () => {
-            console.log(`[subscribe ${userId}] end`);
-        });
-    }
-}
+    out.close();
+    return;
+  }
+};
--- index.js
+++ index.js
@@ -1,50 +1,90 @@
 const Client = require("./Client");
+const { ChannelType } = require("discord.js");
 const { joinVoiceChannel } = require("@discordjs/voice");
 
 const client = new Client();
 
-const ids = ["" /* Player id */, "" /* DM id just in case */];
+const ids = ["865033209380995122" /* Player id */, "" /* DM id just in case */];
+const ignore = []; // array of user ids to ignore when getting words
 const prefix = "!";
 
-client.on("messageCreate", (message) => {
-    if(message.author.id == client.user.id) return;
-    if(!ids.includes(message.author.id)) return;
-    if(!message.content.startsWith(prefix)) return;
+client.on("messageCreate", async (message) => {
+  if (message.author.id == client.user.id) return;
+  if (!ids.includes(message.author.id)) return;
+  if (!message.content.startsWith(prefix)) return;
 
-    const command = message.content.slice(prefix.length);
-    const name = command.split(" ")[0];
-    const args = command.split(" ").slice(1);
+  const command = message.content.slice(prefix.length);
+  const name = command.split(" ")[0];
+  const args = command.split(" ").slice(1);
 
-    switch(name.toLowerCase()) {
-        case "join": {
-            const voiceChannel = message.guild.channels.cache
-                .filter(c => c.type === ChannelType.GuildVoice)
-                .find(channel => channel.id === args[0]);
-            
-            if(!voiceChannel) return message.reply(`There is no voice channel with the id of ${args[0]}`);
-        
-            if(voiceChannel.joinable) {
-                const conn = joinVoiceChannel({
-                    adapterCreator: message.guild.voiceAdapterCreator,
-                    guildId: message.guild.id,
-                    channelId: voiceChannel.id
-                });
+  switch (name.toLowerCase()) {
+    case "norec": {
+      const channel = message.member.voice.channel;
+      if (channel.joinable) {
+        const conn = await joinVoiceChannel({
+          adapterCreator: message.guild.voiceAdapterCreator,
+          guildId: message.guildId,
+          channelId: channel.id,
+          selfDeaf: false,
+          selfMute: false,
+        });
 
-                client.connection = conn;
-
-                voiceChannel.members.filter((m) => m.id !== client.user.id).forEach((member) => {
-                    client.subscribe(member.id);
-                })
-            }
-        }
+        client.connection = conn;
+      }
+      break;
     }
+    case "join": {
+      let voiceChannel = message.guild.channels.cache
+        .filter((c) => c.type === ChannelType.GuildVoice)
+        .find((channel) => channel.id === args[0]);
+
+      if (!voiceChannel && message.member.voice.channel)
+        voiceChannel = message.member.voice.channel;
+
+      if (!voiceChannel)
+        return message.reply(
+          `There is no voice channel with the id of ${args[0]}`
+        );
+
+      if (voiceChannel.joinable) {
+        const conn = await joinVoiceChannel({
+          adapterCreator: message.guild.voiceAdapterCreator,
+          guildId: message.guild.id,
+          channelId: voiceChannel.id,
+          selfDeaf: false,
+        });
+
+        client.connection = conn;
+
+        voiceChannel.members
+          .filter((m) => m.id !== client.user.id)
+          .filter((m) => !ignore.includes(m))
+          .forEach((member) => {
+            client.subscribe(member.id);
+          });
+      }
+
+      break;
+    }
+    case "say":
+    case "play":
+      client.say(args.join(" "));
+      break;
+  }
 });
 
 client.on("voiceStateUpdate", (oldState, newState) => {
-    if(newState.channelId == null) {
-        client.connection.receiver.subscriptions.get(oldState.member.id).destroy();
-    }
-    if(oldState.channelId == null) {
-        client.subscribe(oldState.member.id);
-    }
+  if (oldState.member.id == client.user.id) return;
+  if (newState.channelId == null && client.connection) {
+    client.connection.receiver.subscriptions.get(oldState.member.id).destroy();
+  }
+  if (oldState.channelId == null) {
+    client.subscribe(oldState.member.id);
+  }
 });
+
+client.on("ready", () => {
+  console.log(`${client.user.username} is ready`);
+});
+
+client.login();
